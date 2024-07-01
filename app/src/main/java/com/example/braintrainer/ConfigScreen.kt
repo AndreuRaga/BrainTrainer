ff89cbc3345ae680ad7ce2c3a8b2d6662c289dfd
AndreuRaga <andreurag@gmail.com>
2024-07-01 19:28:36 +0200

    Creada pantalla de configuración

---
+++ app/src/main/java/com/example/braintrainer/ConfigScreen.kt
@@ -0,0 +1,37 @@
+package com.example.braintrainer
+
+import android.graphics.Bitmap.Config
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ConfigScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { /* Acción al hacer clic */ }) {
+            Text("Cerrar sesión")
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun ConfigScreenPreview() {
+    ConfigScreen()
+}
