58030f678465941cc7168113221355d2c4d996ca
AndreuRaga <andreurag@gmail.com>
2024-07-01 15:14:54 +0200

    Librerías de Google Authentication añadidas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ android {
 dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
+    implementation(libs.firebase.auth.ktx)
+    implementation(libs.play.services.auth)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
