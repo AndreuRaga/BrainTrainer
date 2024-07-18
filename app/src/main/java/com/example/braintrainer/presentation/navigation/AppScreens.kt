e0264903902e511e585619e37d5d6e651fc48642
AndreuRaga <andreurag@gmail.com>
2024-07-18 17:44:28 +0200

    Ruta a Pantalla de splash eliminada

--- app/src/main/java/com/example/braintrainer/presentation/navigation/AppScreens.kt
+++ app/src/main/java/com/example/braintrainer/presentation/navigation/AppScreens.kt
@@ -1,7 +1,6 @@
 package com.example.braintrainer.presentation.navigation
 
 sealed class AppScreens(val route: String) {
-    object SplashScreen : AppScreens("splash_screen")
     object AuthScreen : AppScreens("auth_screen")
     object ConfigScreen : AppScreens("config_screen")
 }
