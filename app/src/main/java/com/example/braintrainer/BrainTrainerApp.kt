3a897c09ff4a57c3fc99f37c3c8f3266d55f9d87
AndreuRaga <andreurag@gmail.com>
2024-07-19 16:06:48 +0200

    Refactorización de toda la app siguiendo la arquitectura recomendada por Android

---
+++ app/src/main/java/com/example/braintrainer/BrainTrainerApp.kt
@@ -0,0 +1,9 @@
+package com.example.braintrainer
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BrainTrainerApp : Application() {
+    // Puedes añadir código de inicialización aquí si es necesario
+}
