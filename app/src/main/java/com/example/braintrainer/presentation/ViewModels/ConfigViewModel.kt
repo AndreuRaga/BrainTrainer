b44f06a73b4c82e7f473e370b4befb0c1bfd7d6f
AndreuRaga <andreurag@gmail.com>
2024-07-19 17:39:42 +0200

    View Models para las pantallas creados

---
+++ app/src/main/java/com/example/braintrainer/presentation/ViewModels/ConfigViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.braintrainer.presentation.ViewModels
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ConfigViewModel @Inject constructor() : ViewModel() {
+}
