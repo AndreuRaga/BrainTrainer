0c5bdc6f32dcf462f5c911b3975995fb3310f0e1
AndreuRaga <andreurag@gmail.com>
2024-07-19 17:23:48 +0200

    Empezando a implemntar Hilt

--- app/src/main/java/com/example/braintrainer/presentation/MainActivity.kt
+++ app/src/main/java/com/example/braintrainer/presentation/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.braintrainer.presentation.navigation.AppNavigation
 import com.example.braintrainer.ui.theme.BrainTrainerTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
