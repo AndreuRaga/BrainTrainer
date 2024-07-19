b1eeb17652066d4e6f1d69b59e24a159426719bc
AndreuRaga <andreurag@gmail.com>
2024-07-19 16:50:59 +0200

    Librerías actualizadas

--- app/src/main/java/com/example/braintrainer/BrainTrainerApp.kt
+++
@@ -1,9 +0,0 @@
-package com.example.braintrainer
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class BrainTrainerApp : Application() {
-    // Puedes añadir código de inicialización aquí si es necesario
-}
