559719a3c5f3078a91cba3163166f3213e8cfbac
AndreuRaga <andreurag@gmail.com>
2024-07-26 20:42:57 +0200

    Nada

--- app/src/main/java/com/example/braintrainer/presentation/navigation/AppScreens.kt
+++ app/src/main/java/com/example/braintrainer/presentation/navigation/AppScreens.kt
@@ -2,7 +2,7 @@ package com.example.braintrainer.presentation.navigation
 
 sealed class AppScreens(val route: String) {
     object AuthScreen : AppScreens("auth_screen")
-    object ConfigScreen : AppScreens("config_screen")
     object GamesScreen : AppScreens("games_screen")
     object StatsScreen : AppScreens("stats_screen")
+    object ConfigScreen : AppScreens("config_screen")
 }
