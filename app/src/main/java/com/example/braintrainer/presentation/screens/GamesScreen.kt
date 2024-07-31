72eeed59ea92b5b16ca95ab7e6cc223030809a8a
AndreuRaga <andreurag@gmail.com>
2024-07-31 20:46:29 +0200

    UI mejorada para GamesScreen 2

--- app/src/main/java/com/example/braintrainer/presentation/screens/GamesScreen.kt
+++ app/src/main/java/com/example/braintrainer/presentation/screens/GamesScreen.kt
@@ -28,7 +28,7 @@ fun GamesScreen(navController: NavHostController) {
     Scaffold(bottomBar = { BottomBarMenu(navController) }) {
         innerPadding ->
         LazyColumn(contentPadding = PaddingValues(
-            top = 16.dp,
+            top = innerPadding.calculateTopPadding() + 16.dp,
             start = 16.dp,
             end = 16.dp,
             bottom = innerPadding.calculateBottomPadding() + 16.dp
