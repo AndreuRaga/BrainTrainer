9e1edd30a8a49e33aa57da791c07cc390946ea22
AndreuRaga <andreurag@gmail.com>
2024-08-01 19:48:44 +0200

    UI básica para EndGameScreen

--- app/src/main/java/com/example/braintrainer/presentation/screens/EndGameScreen.kt
+++ app/src/main/java/com/example/braintrainer/presentation/screens/EndGameScreen.kt
@@ -1,13 +1,47 @@
 package com.example.braintrainer.presentation.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.braintrainer.presentation.navigation.AppScreens
 
 @Composable
 fun EndGameScreen(navController: NavHostController) {
-    Column {
-        Text("Pantalla de finalización del juego")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Mejores puntuaciones:",
+            style = MaterialTheme.typography.headlineSmall
+        )
+        Text("1. 100 puntos")
+        Text("2. 80 puntos")
+        Text("3. 50 puntos")
+        Spacer(modifier = Modifier.height(32.dp))
+        Text(
+            text = "¡Enhorabuena!",
+            style = MaterialTheme.typography.headlineLarge,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { navController.navigate(AppScreens.GamesScreen.route) }) {
+            Text("Jugar a otros juegos")
+        }
     }
 }
