b71f3b324de68048d4120f20b49a9d23e0ba962b
AndreuRaga <andreurag@gmail.com>
2024-08-01 19:41:45 +0200

    Mensaje para confirmar el abandono del juego

--- app/src/main/java/com/example/braintrainer/presentation/screens/PlayScreen.kt
+++ app/src/main/java/com/example/braintrainer/presentation/screens/PlayScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -14,6 +15,10 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -23,6 +28,7 @@ import com.example.braintrainer.presentation.navigation.AppScreens
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PlayScreen(navController: NavHostController, gameName: String) {
+    var showDialog by remember { mutableStateOf(false) }
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -30,7 +36,7 @@ fun PlayScreen(navController: NavHostController, gameName: String) {
                     Text(gameName)
                 },
                 navigationIcon = {
-                    IconButton(onClick = { navController.popBackStack() }) {
+                    IconButton(onClick = { showDialog = true }) {
                         Icon(
                             imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                             contentDescription = "Volver")
@@ -50,6 +56,24 @@ fun PlayScreen(navController: NavHostController, gameName: String) {
             Text("Jugar a $gameName")
             Button(onClick = { navController.navigate(AppScreens.EndGameScreen.route) }) {
                 Text("Mostrar resultados")
+            }
+            if (showDialog) {
+                AlertDialog(
+                    onDismissRequest = { showDialog = false },
+                    title = { Text("¡Atención!") },
+                    text = { Text("¿Seguro de que quieres salir? Se perderán los puntos que tengas acumulados hasta este momento.") },
+                    confirmButton = {
+                        Button(onClick = {
+                            navController.popBackStack()
+                            showDialog = false
+                        } ) {
+                            Text("Sí")
+                        }
+                    },
+                    dismissButton = { Button(onClick = { showDialog = false } ) {
+                        Text("No")
+                    } }
+                )
             }
         }
     }
