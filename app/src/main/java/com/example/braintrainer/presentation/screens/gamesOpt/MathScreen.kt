6361448c0e7f493c0e6f04c78fb49c61438fa27b
AndreuRaga <andreurag@gmail.com>
2024-08-07 15:27:37 +0200

    UI básica para MathScreen

--- app/src/main/java/com/example/braintrainer/presentation/screens/gamesOpt/MathScreen.kt
+++ app/src/main/java/com/example/braintrainer/presentation/screens/gamesOpt/MathScreen.kt
@@ -2,14 +2,22 @@ package com.example.braintrainer.presentation.screens.gamesOpt
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
 @Composable
@@ -26,5 +34,42 @@ fun MathScreen(navController: NavHostController) {
         Button(onClick = { navController.navigate("end_game_screen") }) {
             Text("Finalizar juego")
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MathScreenPreview() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row {
+            Text("30s")
+            Spacer(modifier = Modifier.width(16.dp))
+            Text("Puntos: 0")
+        }
+        Text("10 + 5", fontSize = 30.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Button(onClick = { /*TODO*/ }) { Text("15") }
+            Button(onClick = { /*TODO*/ }) { Text("20") }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Button(onClick = { /*TODO*/ }) { Text("10") }
+            Button(onClick = { /*TODO*/ }) { Text("5") }
+        }
+        Text("¡Correcto!", fontSize = 20.sp, color = Color.Green)
+        Text("¡Incorrecto!", fontSize = 20.sp, color = Color.Red)
     }
 }
