531f0f44e9f0d725c307b4e01debd3dd9114b267
AndreuRaga <andreurag@gmail.com>
2024-08-10 13:39:42 +0200

    MatableLista a List

--- app/src/main/java/com/example/braintrainer/presentation/uiStates/MathUiState.kt
+++ app/src/main/java/com/example/braintrainer/presentation/uiStates/MathUiState.kt
@@ -3,7 +3,7 @@ package com.example.braintrainer.presentation.uiStates
 data class MathUiState(
     val num1: Int = 0,
     val num2: Int = 0,
-    val answers: MutableList<Int> = mutableListOf(0, 0, 0, 0),
+    val answers: List<Int> = listOf(0, 0, 0, 0),
     val points: Int = 0,
     val timer: Int = 35,
     val currentOperation: Int = 1,
