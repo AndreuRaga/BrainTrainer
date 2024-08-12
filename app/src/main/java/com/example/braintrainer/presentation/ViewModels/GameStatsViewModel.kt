e6532ebe33a658759f005a0d65cd71b832c7de31
AndreuRaga <andreurag@gmail.com>
2024-08-12 17:58:26 +0200

    Refactoring de GameStatsScreen con la arquitectura Android

---
+++ app/src/main/java/com/example/braintrainer/presentation/ViewModels/GameStatsViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.braintrainer.presentation.ViewModels
+
+import androidx.lifecycle.ViewModel
+import com.example.braintrainer.data.models.GameCategory
+import com.example.braintrainer.data.repositories.GameCategoryRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class GameStatsViewModel @Inject constructor(
+    private val gameCategoryRepository: GameCategoryRepository
+) : ViewModel() {
+    fun getGameCategories(): List<GameCategory> {
+        return gameCategoryRepository.getGameCategories()
+    }
+}
