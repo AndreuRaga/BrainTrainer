e44c401e45297a6024d588b7da7b1378d0b964e5
AndreuRaga <andreurag@gmail.com>
2024-08-12 18:32:13 +0200

    Cambio visual en el nombre del juego

--- app/src/main/java/com/example/braintrainer/presentation/screens/gamesOpt/PlayScreen.kt
+++ app/src/main/java/com/example/braintrainer/presentation/screens/gamesOpt/PlayScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +22,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
@@ -35,7 +37,11 @@ fun PlayScreen(navController: NavHostController, playViewModel: PlayViewModel = 
         topBar = {
             CenterAlignedTopAppBar(
                 title = {
-                    Text(game?.name ?: "Juego no encontrado")
+                    Text(
+                        text = game?.name ?: "Juego no encontrado",
+                        style = MaterialTheme.typography.headlineMedium,
+                        fontWeight = FontWeight.Bold
+                    )
                 },
                 navigationIcon = {
                     IconButton(onClick = { showDialog = true }) {
