8af70cd0b5224cf3ec51a9702a102caa212f4b8d
AndreuRaga <andreurag@gmail.com>
2024-08-20 20:52:43 +0200

    Reformat code

--- app/src/main/java/com/example/braintrainer/AppModule.kt
+++ app/src/main/java/com/example/braintrainer/AppModule.kt
@@ -6,19 +6,19 @@ import com.example.braintrainer.data.dataSources.GameCategoryDataSource
 import com.example.braintrainer.data.dataSources.GameCategoryDataSourceImpl
 import com.example.braintrainer.data.dataSources.ScoreDataSource
 import com.example.braintrainer.data.dataSources.ScoreDataSourceImpl
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
-import com.example.braintrainer.data.repositories.AuthRepositoryImpl
-import com.example.braintrainer.data.repositories.UserRepositoryImpl
 import com.example.braintrainer.data.repositories.AuthRepository
+import com.example.braintrainer.data.repositories.AuthRepositoryImpl
 import com.example.braintrainer.data.repositories.GameCategoryRepository
 import com.example.braintrainer.data.repositories.GameCategoryRepositoryImpl
 import com.example.braintrainer.data.repositories.ScoreRepository
 import com.example.braintrainer.data.repositories.ScoreRepositoryImpl
 import com.example.braintrainer.data.repositories.UserRepository
+import com.example.braintrainer.data.repositories.UserRepositoryImpl
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -48,7 +48,8 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideAuthRepository(firebaseAuth: FirebaseAuth): AuthRepository = AuthRepositoryImpl(firebaseAuth)
+    fun provideAuthRepository(firebaseAuth: FirebaseAuth): AuthRepository =
+        AuthRepositoryImpl(firebaseAuth)
 
     @Provides
     @Singleton
@@ -56,13 +57,16 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideGameCategoryRepository(dataSource: GameCategoryDataSource): GameCategoryRepository = GameCategoryRepositoryImpl(dataSource)
+    fun provideGameCategoryRepository(dataSource: GameCategoryDataSource): GameCategoryRepository =
+        GameCategoryRepositoryImpl(dataSource)
 
     @Provides
     @Singleton
-    fun provideScoreRepository(dataSource: ScoreDataSource): ScoreRepository = ScoreRepositoryImpl(dataSource)
+    fun provideScoreRepository(dataSource: ScoreDataSource): ScoreRepository =
+        ScoreRepositoryImpl(dataSource)
 
     @Provides
     @Singleton
-    fun provideCredentialManager(@ApplicationContext context: Context): CredentialManager = CredentialManager.create(context)
+    fun provideCredentialManager(@ApplicationContext context: Context): CredentialManager =
+        CredentialManager.create(context)
 }
