b9f04041cf4ef3f72af36663a029f7db25e0f28b
AndreuRaga <andreurag@gmail.com>
2024-08-20 20:01:37 +0200

    StatsUiState.kt: Refactoring

--- app/src/main/java/com/example/braintrainer/presentation/uiStates/StatsUiState.kt
+++ app/src/main/java/com/example/braintrainer/presentation/uiStates/StatsUiState.kt
@@ -4,15 +4,17 @@ import com.example.braintrainer.data.models.GameCategory
 
 sealed class StatsUiState {
     object Loading : StatsUiState()
+
     data class Success(
         val categories: List<GameCategory>,
         val overallPerformance: OverallPerformance? = null
     ) : StatsUiState()
+
     data class Error(val message: String) : StatsUiState()
 }
 
 data class OverallPerformance(
     val totalBestScore: Int,
-    val totalMaxScore:Int,
+    val totalMaxScore: Int,
     val progress: Float
 )
