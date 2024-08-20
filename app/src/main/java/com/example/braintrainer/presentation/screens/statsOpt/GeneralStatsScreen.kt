5f94ab0ffccc389bbce09f3764cd8254fa9769cc
AndreuRaga <andreurag@gmail.com>
2024-08-20 11:10:43 +0200

    GeneralStatsScreen.kt: UI para los puntos y el rendimiento

--- app/src/main/java/com/example/braintrainer/presentation/screens/statsOpt/GeneralStatsScreen.kt
+++ app/src/main/java/com/example/braintrainer/presentation/screens/statsOpt/GeneralStatsScreen.kt
@@ -1,5 +1,6 @@
 package com.example.braintrainer.presentation.screens.statsOpt
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -7,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.HorizontalDivider
@@ -48,22 +48,25 @@ fun CategoryStatsItem(categoryName: String) {
             .fillMaxWidth()
             .padding(vertical = 8.dp)
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
         ) {
-            Text(
-                text = categoryName,
-                modifier = Modifier.weight(1f)
+            Text(text = categoryName)
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("X punto(s)")
+                Text("Rendimiento: Y")
+            }
+            Spacer(modifier = Modifier.height(4.dp))
+            LinearProgressIndicator(
+                progress = { 0.8f },
+                modifier = Modifier.fillMaxWidth(),
             )
-            Spacer(modifier = Modifier.width(8.dp))
-            Text("80%") // Reemplazar con la puntuación promedio real
         }
-        Spacer(modifier = Modifier.height(4.dp))
-        LinearProgressIndicator(
-            progress = {
-                0.8f // Reemplazar con el progreso real
-            },
-            modifier = Modifier.fillMaxWidth(),
-        )
     }
 }
