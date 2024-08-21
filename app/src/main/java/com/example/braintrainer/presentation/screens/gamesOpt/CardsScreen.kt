6a6a27343197ae3ce46ccb7f8a0c65a5606e9d04
AndreuRaga <andreurag@gmail.com>
2024-08-22 00:08:39 +0200

    CardsScreen.kt: UI básica

--- app/src/main/java/com/example/braintrainer/presentation/screens/gamesOpt/CardsScreen.kt
+++ app/src/main/java/com/example/braintrainer/presentation/screens/gamesOpt/CardsScreen.kt
@@ -1,8 +1,26 @@
 package com.example.braintrainer.presentation.screens.gamesOpt
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.braintrainer.presentation.ViewModels.CardsViewModel
@@ -10,5 +28,49 @@ import com.example.braintrainer.presentation.ViewModels.CardsViewModel
 @Composable
 fun CardsScreen(navController: NavHostController, cardsViewModel: CardsViewModel = hiltViewModel()) {
     val uiState = cardsViewModel.uiState.collectAsState()
-    Text(text = "Cards Screen")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth().padding(16.dp)
+        ) {
+            Text("Puntos: 0")
+            Text("Intentos: 1/25")
+        }
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(4),
+            contentPadding = PaddingValues(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(getCards()) { card ->
+                Card(
+                    modifier = Modifier.aspectRatio(1f),
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color.LightGray
+                    )
+                ) {
+                    Button(
+                        onClick = { /* Acción al pulsar el botón */ },
+                        modifier = Modifier.fillMaxSize(),
+                        contentPadding = PaddingValues(0.dp)
+                    ) {
+                        // Aquí puedes mostrar la imagen correspondiente a la carta
+                        // Por ejemplo,usando una Image con la URL de la imagen
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+// Función para obtener las cartas (reemplaza con tu lógica)
+fun getCards(): List<Int> {
+    return (1..16).toList()
 }
