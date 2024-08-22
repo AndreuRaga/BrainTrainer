1119a793cb71b72858bbcf4b405213d709ce9ae0
AndreuRaga <andreurag@gmail.com>
2024-08-22 20:10:20 +0200

    CardsScreen.kt: Las cartas se giran lo de un segundo

--- app/src/main/java/com/example/braintrainer/presentation/screens/gamesOpt/CardsScreen.kt
+++ app/src/main/java/com/example/braintrainer/presentation/screens/gamesOpt/CardsScreen.kt
@@ -18,7 +18,13 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,6 +35,13 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.braintrainer.R
 import com.example.braintrainer.presentation.ViewModels.CardsViewModel
+import kotlinx.coroutines.delay
+
+data class CardData(
+    val id: Int,
+    val image: Int,
+    var isRevealed: Boolean = false
+)
 
 @Composable
 fun CardsScreen(
@@ -36,6 +49,24 @@ fun CardsScreen(
     cardsViewModel: CardsViewModel = hiltViewModel()
 ) {
     val uiState = cardsViewModel.uiState.collectAsState()
+    val cards = remember { mutableStateListOf<CardData>() }
+
+    // Inicializar las cartas al inicio
+    LaunchedEffect(Unit) {
+        val images = listOf(
+            R.drawable.card0,
+            R.drawable.card1,
+            R.drawable.card2,
+            R.drawable.card3,
+            R.drawable.card4,
+            R.drawable.card5,
+            R.drawable.card6,
+            R.drawable.card7
+        )
+        val pairs = (images + images).shuffled()
+        cards.addAll(pairs.mapIndexed { index, image -> CardData(index, image) })
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -58,48 +89,48 @@ fun CardsScreen(
             verticalArrangement = Arrangement.spacedBy(8.dp),
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            items(getCards()) { card ->
-                Card(
-                    modifier = Modifier.aspectRatio(1f),
-                    shape = RectangleShape,
-                    colors = CardDefaults.cardColors(
-                        containerColor = Color.LightGray
-                    )
-                ) {
-                    Button(
-                        onClick = { /* Acción al hacer clic en la carta */ },
-                        modifier = Modifier.fillMaxSize(),
-                        contentPadding = PaddingValues(0.dp),
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = Color.Transparent
-                        ),
-                        shape = RectangleShape,
-                        border = ButtonDefaults.outlinedButtonBorder
-
-                    ) {
-                        Image(
-                            painter = painterResource(id = card), // Mostrar la imagen correspondiente
-                            contentDescription = "Imagen que funciona como botón",
-                            modifier = Modifier.fillMaxSize()
-                        )
-                    }
-                }
+            items(cards) { card ->
+                CardItem(card)
             }
         }
     }
 }
 
-// Función para obtener las cartas
-fun getCards(): List<Int> {
-    val cards = listOf(
-        R.drawable.card0,
-        R.drawable.card1,
-        R.drawable.card2,
-        R.drawable.card3,
-        R.drawable.card4,
-        R.drawable.card5,
-        R.drawable.card6,
-        R.drawable.card7
-    )
-    return (cards + cards).shuffled() // Duplicar y barajar las cartas
+@Composable
+fun CardItem(card: CardData) {
+    var isRevealed by remember { mutableStateOf(true) } // Estado de la carta
+
+    Card(
+        modifier = Modifier.aspectRatio(1f),
+        shape = RectangleShape,
+        colors = CardDefaults.cardColors(
+            containerColor = Color.LightGray
+        )
+    ) {
+        Button(
+            onClick = { isRevealed = !isRevealed },
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(0.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent
+            ),
+            shape = RectangleShape,
+            border = ButtonDefaults.outlinedButtonBorder
+
+        ) {
+            Image(
+                painter = painterResource(id = if (isRevealed) card.image else R.drawable.card_background),
+                contentDescription = "Imagen que funciona como botón",
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+    }
+
+    // Dar la vuelta a la carta después de un tiempo
+    LaunchedEffect(key1 = isRevealed) {
+        if (isRevealed) {
+            delay(1000L) // Esperar 1 segundo
+            isRevealed = false
+        }
+    }
 }
