a7c0b91a8cbdcd06e744cd7a345293a090a84a31
AndreuRaga <andreurag@gmail.com>
2024-08-26 16:30:40 +0200

    GameCategoryRepositoryImpl.kt: Funciones no usadas borradas

--- app/src/main/java/com/example/braintrainer/data/repositories/GameCategoryRepository.kt
+++ app/src/main/java/com/example/braintrainer/data/repositories/GameCategoryRepository.kt
@@ -4,8 +4,6 @@ import com.example.braintrainer.data.models.Game
 import com.example.braintrainer.data.models.GameCategory
 
 interface GameCategoryRepository {
-    fun getGameCategories(): List<GameCategory>
     suspend fun getCategoriesFromDB(): Result<List<GameCategory>>
     suspend fun getGameByIdFromDB(gameId: String): Result<Game?>
-    fun getGameById(gameId: String): Game?
 }
--- app/src/main/java/com/example/braintrainer/data/repositories/GameCategoryRepositoryImpl.kt
+++ app/src/main/java/com/example/braintrainer/data/repositories/GameCategoryRepositoryImpl.kt
@@ -3,16 +3,11 @@ package com.example.braintrainer.data.repositories
 import com.example.braintrainer.data.dataSources.GameCategoryDataSource
 import com.example.braintrainer.data.models.Game
 import com.example.braintrainer.data.models.GameCategory
-import com.example.braintrainer.data.models.GameData
 import javax.inject.Inject
 
 
 class GameCategoryRepositoryImpl @Inject constructor(private val dataSource: GameCategoryDataSource) :
     GameCategoryRepository {
-    override fun getGameCategories(): List<GameCategory> {
-        return GameData.categories
-    }
-
     override suspend fun getCategoriesFromDB(): Result<List<GameCategory>> {
         return dataSource.getCategories()
     }
@@ -21,9 +16,5 @@ class GameCategoryRepositoryImpl @Inject constructor(private val dataSource: Gam
         val categories = dataSource.getCategories().getOrThrow()
         val game = categories.flatMap { it.games }.find { it.id == gameId }
         return Result.success(game)
-    }
-
-    override fun getGameById(gameId: String): Game? {
-        return GameData.categories.flatMap { it.games }.find { it.id == gameId }
     }
 }
