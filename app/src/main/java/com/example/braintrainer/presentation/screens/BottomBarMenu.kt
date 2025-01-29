4b00a26ae8427773ebeb9603c521318e64b4a625
AndreuRaga <andreurag@gmail.com>
2025-01-29 19:31:31 +0100

    BottomBarMenu.kt: Color blanco para item seleccionado

--- app/src/main/java/com/example/braintrainer/presentation/screens/BottomBarMenu.kt
+++ app/src/main/java/com/example/braintrainer/presentation/screens/BottomBarMenu.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -52,7 +53,8 @@ fun BottomBarMenu(navController: NavHostController) {
                             fontWeight = FontWeight.Bold
                         )
                     }
-                }
+                },
+                colors = NavigationBarItemDefaults.colors(indicatorColor = Color.White)
             )
         }
     }
